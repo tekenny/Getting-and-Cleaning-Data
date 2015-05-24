2e82b33f05fc290b49934a773480a442c3cfc636
Thomas Kenny <thomas.e.kenny@gmail.com>
2015-05-23 20:06:04 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
 library(plyr)
+library(dplyr)
+
+# Creates a second independent tidy data set with the average of each variable
+# for each activity and each subject
+average_each_activity_and_each_subject <- function(dataset) {
+    dataset    
+}
 
 # Download the zip file from the Internet which contains the raw data
 # and then extract (unzip) the data from the zip file
@@ -19,6 +26,26 @@ download_raw_data <- function() {
     }
 }
 
+# Extract only the measurements on the mean and standard deviation for
+# each measurement
+extract_mean_and_standard_deviation <- function(dataset) {
+    # Read features for use as column names of X data variables
+    features <- read.table("UCI-HAR-Dataset/features.txt")
+    
+    # Select columns with the mean function it it's name
+    meanColumns <- grep(pattern = "-mean()", features$V2, fixed=TRUE, value=FALSE)
+
+    # Select columns with the std (standard deviation) function in it's name
+    stdColumns <- grep(pattern = "-std()",  features$V2, fixed=TRUE, value=FALSE)
+    
+    # Merge all the selected columns together
+    columns <- dim(dataset)[2]
+    selectedColumns <- c(meanColumns, stdColumns, columns-1, columns)
+    
+    # Now actually select the desired columns in the dataset
+    dataSubset <- dataset[, as.vector(selectedColumns)]
+}
+
 # Read the raw data from seperate files and merge it into one dataset
 read_raw_data <- function() {
     # Read the test data
@@ -30,11 +57,8 @@ read_raw_data <- function() {
     stopifnot(dim(test.s)[1] == dim(test.x)[1])
     stopifnot(dim(test.x)[1] == dim(test.y)[1])
     
-    # Append a category column containing "test"
-    # in case distinguishing between test and train data may be useful
-    test.y$Category <- "test"
-    test            <- cbind(test.s, test.y, test.x)
-
+    test <- cbind(test.x, test.s, test.y)
+    
     # Read the train data
     train.s <- read.table("UCI-HAR-Dataset/train/subject_train.txt")
     train.x <- read.table("UCI-HAR-Dataset/train/X_train.txt")
@@ -44,15 +68,21 @@ read_raw_data <- function() {
     stopifnot(dim(train.s)[1] == dim(train.x)[1])  
     stopifnot(dim(train.x)[1] == dim(train.y)[1])
     
-    # Append a category column containing "train"
-    # in case distinguishing between test and train data may be useful
-    train.y$Category <- "train"
-    train            <- cbind(train.s, train.y, train.x)
+    train <- cbind(train.x, train.s, train.y)
     
     # Combine the test and train data into one dataset
     dataset <- rbind(test, train)
+    
+    # Set the column names for the last two columns
+    columns <- dim(dataset)[2]
+    colnames(dataset)[columns-1] = "Subject"
+    colnames(dataset)[columns] = "Activity"
+    
+    dataset
 }
 
+# Mutate the Activity column in the dataset from activity code values
+# to names indicating the activity
 mutate_activity <- function(dataset) {
     # Read activity labels to subsitute codes for descriptive names
     activity_labels <- read.table("UCI-HAR-Dataset/activity_labels.txt")
@@ -67,17 +97,25 @@ mutate_activity <- function(dataset) {
 # Note: intentionally kept special characters in the column names
 #       to provide a more descriptive name. Such as () suffixing the name of
 #       the function that was used to derive values
-set_column_names <- function(dataset) {
-    # Set column names for non-X data
-    colnames(dataset)[1:3] <- c("Subject", "Activity", "Category")
-    
+set_column_names <- function(dataset) {    
     # Read features for use as column names of X data variables
     features <- read.table("UCI-HAR-Dataset/features.txt")
     
-    # Set column names for X data
-    colnames(dataset)[4:564] <- as.character(features[,2])
+    # Select column names with the mean function it it's name
+    meanColumnNames <- grep(pattern = "-mean()", features$V2, fixed=TRUE, value=TRUE)
     
-    dataset
+    # Select column names with the std (standard deviation) function in it's name
+    stdColumnNames <- grep(pattern = "-std()",  features$V2, fixed=TRUE, value=TRUE)
+    
+    # Merge all the selected column names together
+    selectedColumnNames <- c(meanColumnNames, stdColumnNames, "Subject", "Activity")
+    
+    # Now actually set the column names to the dataset
+    colnames(dataset) <- selectedColumnNames
+
+    # Reorganize columns so Subject and Activity are 1st two columns
+    columns <- dim(dataset)[2]
+    dataset[, c(columns-1,columns,1:(columns-2))]
 }
 
 # Main line code starts here
@@ -85,19 +123,20 @@ set_column_names <- function(dataset) {
 # Download the raw data from the Internet
 download_raw_data()
 
-# Read and then merge the test and the train raw data to create one data set
+# 1 Read and then merge the test and the train raw data to create one data set
 dataset <- read_raw_data()
 
-# Appropriately label the data set with descriptive variable names 
-dataset <- set_column_names(dataset)
+# 2 Extract only the mean and standard deviation measurements
+dataset <- extract_mean_and_standard_deviation(dataset)
 
-# Mutate activity variables from codes to descriptive activity names
+# 3 Mutate activity variables from codes to descriptive activity names
 dataset <- mutate_activity(dataset)
 
-# 2 - Extracts only the measurements on the mean and standard deviation for
-#     each measurement 
+# 4 Appropriately label the data set with descriptive variable names 
+dataset <- set_column_names(dataset)
 
-# 5 - From the data set in step 4, creates a second, independent tidy data set
-#     with the average of each variable for each activity and each subject
+# 5 Create a tidy data set with the average of each variable for each activity
+#   and each subject
+dataset <- average_each_activity_and_each_subject(dataset)
 
-#write.table("analysis.txt", row.name=FALSE)
+write.table(dataset, "analysis.txt", row.name=FALSE)
